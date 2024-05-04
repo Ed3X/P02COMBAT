0f0ddc5c5dedb887b9201c23a4512720e2f11fa4
Ed3X <edgar.obrador@estudiantat.upc.edu>
2024-05-04 22:31:09 +0200

    ola

--- Assets/Assets/Moñecos/Enemy/Scripts/EnemyMove.cs
+++ Assets/Assets/Moñecos/Enemy/Scripts/EnemyMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
@@ -10,38 +11,91 @@ public class EnemyMove : MonoBehaviour
     public Quaternion angulo;
     public float grado;
 
+    public GameObject target;
+    public bool atacando;
+
+    public NavMeshAgent agente;
+    public float distancia_ataque;
+    public float radio_vision;
+
+    public float speed;
 
     void Start()
     {
         ani = GetComponent<Animator>();
+        target = GameObject.Find("Player");
     }
 
     public void Comportamiento_Enemigo()
     {
-        cronometro += 1 * Time.deltaTime;
-        if (cronometro >= 4)
+        if (Vector3.Distance(transform.position, target.transform.position) > radio_vision)
         {
-            rutina = Random.Range(0, 2);
-            cronometro = 0;
+            agente.enabled = false;
+            ani.SetBool("run", false);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+                    ani.SetBool("walk", false);
+                    break;
+
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 0.5f * Time.deltaTime);
+                    ani.SetBool("walk", true);
+                    break;
+            }
         }
-        switch (rutina)
+        else
         {
-            case 0:
+                var lookPos = target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+
+            agente.enabled = true;
+            agente.SetDestination(target.transform.position);
+
+            if (Vector3.Distance(tranform.position, target.transform.position) > distancia_ataque && !atacando)
+            {
                 ani.SetBool("walk", false);
-                break;
+                ani.SetBool("run", true);
+            }
+            else
+            {
+                if (!atacando)
+                {
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 1);
+                    ani.SetBool("walk", false);
+                    ani.SetBool("run", false);
+                }
+            }
 
-            case 1:
-                grado = Random.Range(0, 360);
-                angulo = Quaternion.Euler(0, grado, 0);
-                rutina++;
-                break;
-
-            case 2:
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 0.5f * Time.deltaTime);
-                ani.SetBool("walk", true);
-                break;
         }
+
+        if(atacando)
+        {
+            agente.enabled = false;
+        }
+    }
+
+    public void Final_Ani()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position > distancia_ataque + 0.2f){
+            ani.SetBool("attack", false);
+        }
+        
+        atacando = false;
     }
 
     void Update()
